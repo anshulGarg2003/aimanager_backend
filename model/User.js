e89e20cf9817657cb0a1aca74f1fdaa2faa0b462
Anshul Garg <anshul2003garg@gmail.com>
2025-03-23 12:32:09 +0530

    Initial commit

---
+++ model/User.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  auth0Id: { type: String, required: true, unique: true },
+  name: String,
+  email: { type: String, required: true, unique: true },
+  picture: String,
+  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
+  completed: [{ type: String }],
+  isPaid: { type: Boolean, default: false },
+  grade: { type: String },
+  school: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
